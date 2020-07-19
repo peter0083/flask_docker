c6b409000213604aca6f7e31c8f5be0d66f82d6b
peter0083 <peter.tingyao@gmail.com>
2020-07-18 23:16:12 -0700

    experiment with google OAuth

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,54 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, session
+from authlib.integrations.flask_client import OAuth
+import os
+
+# OAuth tutorial: https://www.youtube.com/watch?v=BfYsdNaHrps
 
 app = Flask(__name__)
+app.secret_key = "some random secret key"
+oauth = OAuth(app)
+oauth.register(
+    name='google',
+    client_id="some id from google developer api",
+    client_secret="some secret from google developer api",
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
 
-from app import views
+@app.route('/')
+def index():
+    email = dict(session)['profile']['email']
+    return "Hello "+ email +" from Flask app in a Docker container!"
+
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')  # create the google oauth client
+    token = google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    user_info = resp.json()
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session['email'] = user_info['email']
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
